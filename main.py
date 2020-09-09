f6e03c92450d2c40d32d1b53b35eca9d0b30d76f
GtlsGamr <hittarth91@gmail.com>
2020-09-09 21:42:10 +0000

    minor changes

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ import youtube_dl
 import os
 
 if __name__=='__main__':
-    os.remove('t.mp3')
+    # os.remove('t.mp3')
     url = input()
+    print("Enter Artist name: ")
+    artist = input()
+    print("Enter track name: ")
+    track = input()
     ydl_opts = {
-    
-    'format': '140/mp3/bestaudio',       
-    'outtmpl': 't.mp3',               
+
+    'format': '140/mp3/bestaudio',
+    'outtmpl': 't.mp3',
     }
     ydl = youtube_dl.YoutubeDL(ydl_opts)
     ydl.download([url])
     wavewall1 = wavewall(url)
-    width = 1920
-    height = 1080
+    width = 1080*4
+    height = 1920*4
     fimg = wavewall1.graph('t.mp3', (255,255,255))
-    wavewall1.finalimage(fimg,width,height, 'BLKKK SKKKN HEAD / KANYE')
+    a = " ".join(artist)
+    t = " ".join(track)
+    wavewall1.finalimage(fimg,width,height, f'{a} / {t}')
